01ec42167ffcc9325163970f6ade3cc655520eb0
Mathias Polligkeit <git@woylie.de>
2022-09-28 00:47:28 +0900

    update formatter config

--- .formatter.exs
+++ .formatter.exs
@@ -2,6 +2,7 @@
 [
   plugins: [Phoenix.LiveView.HTMLFormatter],
   import_deps: [:phoenix, :phoenix_live_view],
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{heex, ex,exs}"],
-  line_length: 80
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{heex,ex,exs}"],
+  line_length: 80,
+  locals_without_parens: [slot: 2]
 ]
