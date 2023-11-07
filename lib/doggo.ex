da705397780dbece6c52bd7b993c38012a4f2985
Mathias Polligkeit <git@woylie.de>
2023-11-07 18:29:49 +0900

    feat: add modal component

--- lib/doggo.ex
+++ lib/doggo.ex
@@ -145,6 +145,173 @@ defmodule Doggo do
   defp icon_size_class(:normal), do: "is-normal"
 
   @doc """
+  Renders a modal.
+
+  ## Usage
+
+  There are two primary ways to manage the display of the modal: via URL state
+  or by setting and removing the `open` attribute.
+
+  ### With URL
+
+  To toggle the modal visibility based on the URL:
+
+  1. Use the `:if` attribute to conditionally render the modal when a specific
+     live action matches.
+  2. Set the `on_cancel` attribute to patch back to the original URL when the
+     user chooses to close the modal.
+  3. Set the `open` attribute to declare the modal's initial visibility state.
+
+  #### Example
+
+      <.modal
+        :if={@live_action == :show}
+        id="pet-modal"
+        on_cancel={JS.patch(~p"/pets")}
+        open
+      >
+        <:title>Show pet</:title>
+        <p>My pet is called Johnny.</p>
+        <:footer>
+          <.link phx-click={JS.exec("data-cancel", to: "#pet-modal")}>
+            Close
+          </.link>
+        </:footer>
+      </.modal>
+
+  To open the modal, patch or navigate to the URL associated with the live
+  action.
+
+      <.link patch={~p"/pets/\#{@id}"}>show</.link>
+
+  ### Without URL
+
+  To toggle the modal visibility dynamically with the `open` attribute:
+
+  1. Omit the `open` attribute in the template.
+  2. Use the `show_modal` and `hide_modal` functions to change the visibility.
+
+  #### Example
+
+      <.modal id="pet-modal">
+        <:title>Show pet</:title>
+        <p>My pet is called Johnny.</p>
+        <:footer>
+          <.link phx-click={JS.exec("data-cancel", to: "#pet-modal")}>
+            Close
+          </.link>
+        </:footer>
+      </.modal>
+
+  To open modal, use the `show_modal` function.
+
+      <.link phx-click={show_modal("pet-modal")}>show</.link>
+
+  ## CSS
+
+  To hide the modal when the `open` attribute is not set, use the following CSS
+  styles:
+
+      dialog.dg-modal:not([open]),
+      dialog.dg-modal[open="false"] {
+        display: none;
+      }
+
+  ## Semantics
+
+  While the `showModal()` JavaScript function is typically recommended for
+  managing modal dialog semantics, this component utilizes the `open` attribute
+  to control visibility. This approach is chosen to eliminate the need for
+  library consumers to add additional JavaScript code. To ensure proper
+  modal semantics, the `aria-modal` attribute is added to the dialog element.
+
+  """
+
+  attr :id, :string, required: true
+  attr :open, :boolean, default: false, doc: "Initializes the modal as open."
+  attr :on_cancel, JS, default: %JS{}
+
+  slot :title, required: true
+  slot :inner_block, required: true, doc: "The modal body."
+
+  slot :close,
+    doc: "The content for the 'close' link. Defaults to the word 'close'."
+
+  slot :footer
+
+  def modal(assigns) do
+    ~H"""
+    <dialog
+      id={@id}
+      class="dg-modal"
+      aria-modal={(@open && "true") || "false"}
+      aria-labelledby={"#{@id}-title"}
+      open={@open}
+      phx-mounted={@open && show_modal(@id)}
+      phx-remove={hide_modal(@id)}
+      data-cancel={JS.exec(@on_cancel, "phx-remove")}
+    >
+      <.focus_wrap
+        id={"#{@id}-container"}
+        class="dg-modal-container"
+        phx-window-keydown={JS.exec("data-cancel", to: "##{@id}")}
+        phx-key="escape"
+        phx-click-away={JS.exec("data-cancel", to: "##{@id}")}
+      >
+        <article>
+          <header>
+            <.link
+              href="#"
+              class="dg-modal-close"
+              aria-label="Close"
+              phx-click={JS.exec("data-cancel", to: "##{@id}")}
+            >
+              <%= render_slot(@close) %>
+              <span :if={@close == []}>close</span>
+            </.link>
+            <h2 id={"#{@id}-title"}><%= render_slot(@title) %></h2>
+          </header>
+          <div id={"#{@id}-content"} class="dg-modal-content">
+            <%= render_slot(@inner_block) %>
+          </div>
+          <footer :if={@footer != []}>
+            <%= render_slot(@footer) %>
+          </footer>
+        </article>
+      </.focus_wrap>
+    </dialog>
+    """
+  end
+
+  @doc """
+  Shows the modal with the given ID.
+
+  ## Example
+
+      <.link phx-click={show_modal("pet-modal")}>show</.link>
+  """
+  def show_modal(js \\ %JS{}, id) when is_binary(id) do
+    js
+    |> JS.set_attribute({"open", "true"}, to: "##{id}")
+    |> JS.set_attribute({"aria-modal", "true"}, to: "##{id}")
+    |> JS.focus_first(to: "##{id}-content")
+  end
+
+  @doc """
+  Hides the modal with the given ID.
+
+  ## Example
+
+      <.link phx-click={hide_modal("pet-modal")}>hide</.link>
+  """
+  def hide_modal(js \\ %JS{}, id) do
+    js
+    |> JS.remove_attribute("open", to: "##{id}")
+    |> JS.set_attribute({"aria-modal", "false"}, to: "##{id}")
+    |> JS.pop_focus()
+  end
+
+  @doc """
   Renders a list of properties, i.e. key/value pairs.
 
   ## Example
