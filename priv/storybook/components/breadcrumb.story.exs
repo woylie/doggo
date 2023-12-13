65ae95e0c4c6de1e3fd7c14008f70bdcf2358a1a
Mathias Polligkeit <git@woylie.de>
2023-12-13 20:39:11 +0900

    add breadcrumb component

---
+++ priv/storybook/components/breadcrumb.story.exs
@@ -0,0 +1,18 @@
+defmodule Storybook.Components.Breadcrumb do
+  use PhoenixStorybook.Story, :component
+
+  def function, do: &Doggo.breadcrumb/1
+
+  def variations do
+    [
+      %Variation{
+        id: :default,
+        slots: [
+        ~s(<:item patch="/categories">Categories</:item>),
+        ~s(<:item patch="/categories/1">Reviews</:item>),
+        ~s(<:item patch="/categories/1/articles/1">The Movie</:item>)
+        ]
+      }
+    ]
+  end
+end
