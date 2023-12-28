5ffde38149646c44f9dbe7d45460a7c4d2ae9cf6
Mathias Polligkeit <git@woylie.de>
2023-12-28 11:42:52 +0900

    add more test helpers

--- test/support/test_helpers.ex
+++ test/support/test_helpers.ex
@@ -28,6 +28,19 @@ defmodule Doggo.TestHelpers do
   end
 
   @doc """
+  Returns the trimmed text nodes from the first level of the HTML tree returned
+  by the selector.
+
+  Raises if the selector returns zero or more than one result.
+  """
+  def text(html, selector) do
+    html
+    |> find_one(selector)
+    |> Floki.text(deep: false)
+    |> String.trim()
+  end
+
+  @doc """
   Wrapper around `Floki.attribute/2` that unwraps the single attribute.
 
   Raises if the attribute occurs multiple times.
@@ -37,6 +50,38 @@ defmodule Doggo.TestHelpers do
       [value] -> value
       [] -> nil
       _ -> raise "found attribute #{name} multiple times"
+    end
+  end
+
+  @doc """
+  Wrapper around `Floki.attribute/3` that unwraps the single attribute.
+
+  Raises if the attribute occurs multiple times.
+  """
+  def attribute(html, selector, name) do
+    html
+    |> find_one(selector)
+    |> attribute(name)
+  end
+
+  def find_one(html, selector) do
+    case Floki.find(html, selector) do
+      [inner_html] ->
+        inner_html
+
+      [] ->
+        raise """
+        Selector #{inspect(selector)} did not return any results in:
+
+        #{inspect(html, pretty: true)}
+        """
+
+      [_ | _] = results ->
+        raise """
+        Selector #{inspect(selector)} returned multiple results:
+
+        #{inspect(results, pretty: true)}
+        """
     end
   end
 end
