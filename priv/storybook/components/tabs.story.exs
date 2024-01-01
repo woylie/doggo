d1dd1f2c0341592841ac28d91625b7e1ab3de3a1
Mathias Polligkeit <git@woylie.de>
2024-01-01 11:58:06 +0900

    update docs

---
+++ priv/storybook/components/tabs.story.exs
@@ -0,0 +1,42 @@
+defmodule Storybook.Components.Tabs do
+  use PhoenixStorybook.Story, :component
+
+  def function, do: &Doggo.tabs/1
+
+  def variations do
+    [
+      %Variation{
+        id: :default,
+        attributes: %{
+          id: "dog-breed-profiles",
+          title: "Dog Breed Profiles"
+        },
+        slots: [
+          """
+          <:panel label="Golden Retriever">
+            <p>
+              Friendly, intelligent, great with families. Origin: Scotland. Needs
+              regular exercise.
+            </p>
+          </:panel>
+          """,
+          """
+          <:panel label="Siberian Husky">
+            <p>
+              Energetic, outgoing, distinctive appearance. Origin: Northeast Asia.
+              Loves cold climates.
+            </p>
+          </:panel>
+          """,
+          """
+          <:panel label="Dachshund">
+            <p>
+              Playful, stubborn, small size. Origin: Germany. Enjoys sniffing games.
+            </p>
+          </:panel>
+          """
+        ]
+      }
+    ]
+  end
+end
