85599993109da1e128059a4342105004b8afbe54
woylie <13847569+woylie@users.noreply.github.com>
2024-07-18 23:34:59 +0900

    turn split pane into macro

--- lib/doggo/components.ex
+++ lib/doggo/components.ex
@@ -24,6 +24,7 @@ defmodule Doggo.Components do
         disclosure_button()
         fab()
         skeleton()
+        split_pane()
         stack()
         steps()
         switch()
@@ -851,6 +852,136 @@ defmodule Doggo.Components do
   )
 
   component(
+    :split_pane,
+    modifiers: [],
+    doc: """
+    Renders a horizontal or vertical resizable split pane.
+
+    > #### In Development {: .warning}
+    >
+    > The necessary JavaScript for making this component fully functional and
+    > accessible will be added in a future version.
+    >
+    > **Missing features**
+    >
+    > - Resize panes with the mouse
+    > - Resize panes with the keyboard
+    """,
+    usage: """
+    Horizontal separator with label:
+
+    ```heex
+    <.split_pane
+      id="sidebar-splitter"
+      label="Sidebar"
+      orientation="horizontal"
+    >
+      <:primary>One</:primary>
+      <:secondary>Two</:secondary>
+    </.split_pane>
+    ```
+
+    Horizontal separator with visible label:
+
+    ```heex
+    <.split_pane id="sidebar-splitter"
+      labelledby="sidebar-label"
+      orientation="horizontal"
+    >
+      <:primary>
+        <h2 id="sidebar-label">Sidebar</h2>
+        <p>One</p>
+      </:primary>
+      <:secondary>Two</:secondary>
+    </.split_pane>
+    ```
+
+    Nested window splitters:
+
+    ```heex
+    <.split_pane
+      id="sidebar-splitter"
+      label="Sidebar"
+      orientation="horizontal"
+    >
+      <:primary>One</:primary>
+      <:secondary>
+        <.split_pane
+          id="filter-splitter"
+          label="Filters"
+          orientation="vertical"
+        >
+          <:primary>Two</:primary>
+          <:secondary>Three</:secondary>
+        </.split_pane>
+      </:secondary>
+    </.split_pane>
+    ```
+    """,
+    type: :component,
+    since: "0.6.0",
+    attrs_and_slots:
+      quote do
+        attr :label, :string,
+          default: nil,
+          doc: """
+          An accessibility label for the separator if the primary pane has no visible
+          label. If it has a visible label, set the `labelledby` attribute instead.
+
+          Note that the label should describe the primary pane, not the resize handle.
+          """
+
+        attr :labelledby, :string,
+          default: nil,
+          doc: """
+          If the primary pane has a visible label, set this attribute to the DOM ID
+          of that label. Otherwise, provide a label via the `label` attribute.
+          """
+
+        attr :id, :string, required: true
+
+        attr :orientation, :string,
+          values: ["horizontal", "vertical"],
+          required: true
+
+        attr :default_size, :integer, required: true
+        attr :min_size, :integer, default: 0
+        attr :max_size, :integer, default: 100
+        attr :rest, :global, doc: "Any additional HTML attributes."
+
+        slot :primary, required: true
+        slot :secondary, required: true
+      end,
+    heex:
+      quote do
+        Doggo.ensure_label!(var!(assigns), ".split_pane", "Sidebar")
+
+        ~H"""
+        <div
+          id={@id}
+          class={[@base_class | @modifier_classes]}
+          data-orientation={@orientation}
+          {@rest}
+        >
+          <div id={"#{@id}-primary"}><%= render_slot(@primary) %></div>
+          <div
+            role="separator"
+            aria-label={@label}
+            aria-labelledby={@labelledby}
+            aria-controls={"#{@id}-primary"}
+            aria-orientation={@orientation}
+            aria-valuenow={@default_size}
+            aria-valuemin={@min_size}
+            aria-valuemax={@max_size}
+          >
+          </div>
+          <div id={"#{@id}-secondary"}><%= render_slot(@secondary) %></div>
+        </div>
+        """
+      end
+  )
+
+  component(
     :steps,
     modifiers: [],
     extra: [
@@ -1468,7 +1599,7 @@ defmodule Doggo.Components do
       end,
     heex:
       quote do
-        Doggo.ensure_label!(var!(assigns), "tabs", "Dog Facts")
+        Doggo.ensure_label!(var!(assigns), ".tabs", "Dog Facts")
 
         ~H"""
         <div id={@id} class={[@base_class | @modifier_classes]} {@rest}>
@@ -1757,7 +1888,7 @@ defmodule Doggo.Components do
       end,
     heex:
       quote do
-        Doggo.ensure_label!(var!(assigns), "toolbar", "Dog profile actions")
+        Doggo.ensure_label!(var!(assigns), ".toolbar", "Dog profile actions")
 
         ~H"""
         <div
@@ -1964,7 +2095,7 @@ defmodule Doggo.Components do
       end,
     heex:
       quote do
-        Doggo.ensure_label!(var!(assigns), "tree", "Dog Breeds")
+        Doggo.ensure_label!(var!(assigns), ".tree", "Dog Breeds")
 
         ~H"""
         <ul
