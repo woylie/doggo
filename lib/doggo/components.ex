f10d30e7b297158817392e312853a815a33ebf87
woylie <13847569+woylie@users.noreply.github.com>
2024-07-19 18:04:50 +0900

    turn alert dialog into macro

--- lib/doggo/components.ex
+++ lib/doggo/components.ex
@@ -15,6 +15,7 @@ defmodule Doggo.Components do
 
         accordion()
         action_bar()
+        alert_dialog()
         badge()
         box()
         breadcrumb()
@@ -225,6 +226,164 @@ defmodule Doggo.Components do
             <%= render_slot(item) %>
           </button>
         </div>
+        """
+      end
+  )
+
+  component(
+    :alert_dialog,
+    modifiers: [],
+    doc: """
+    Renders an alert dialog that requires the immediate attention and response of
+    the user.
+
+    This component is meant for situations where critical information must be
+    conveyed, and an explicit response is required from the user. It is typically
+    used for confirmation dialogs, warning messages, error notifications, and
+    other scenarios where an immediate decision is necessary.
+
+    For non-critical dialogs, such as those containing forms or additional
+    information, use `Doggo.Components.modal/1` instead.
+    """,
+    usage: """
+    ```heex
+    <.alert_dialog id="end-session-modal">
+      <:title>End Training Session Early?</:title>
+      <p>
+        Are you sure you want to end the current training session with Bella?
+        She's making great progress today!
+      </p>
+      <:footer>
+        <.button phx-click="end-session">
+          Yes, end session
+        </.button>
+        <.button phx-click={JS.exec("data-cancel", to: "#end-session-modal")}>
+          No, continue training
+        </.button>
+      </:footer>
+    </.alert_dialog>
+    ```
+
+    To open the dialog, use the `show_modal/1` function.
+
+    ```heex
+    <.button
+      phx-click={.show_modal("end-session-modal")}
+      aria-haspopup="dialog"
+    >
+      show
+    </.button>
+    ```
+
+    ## CSS
+
+    To hide the modal when the `open` attribute is not set, use the following CSS
+    styles:
+
+    ```css
+    dialog.alert-dialog:not([open]),
+    dialog.alert-dialog[open="false"] {
+      display: none;
+    }
+    ```
+
+    ## Semantics
+
+    While the `showModal()` JavaScript function is typically recommended for
+    managing modal dialog semantics, this component utilizes the `open` attribute
+    to control visibility. This approach is chosen to eliminate the need for
+    library consumers to add additional JavaScript code. To ensure proper
+    modal semantics, the `aria-modal` attribute is added to the dialog element.
+    """,
+    type: :component,
+    since: "0.6.0",
+    attrs_and_slots:
+      quote do
+        attr :id, :string, required: true
+
+        attr :open, :boolean,
+          default: false,
+          doc: "Initializes the dialog as open."
+
+        attr :on_cancel, Phoenix.LiveView.JS,
+          default: %Phoenix.LiveView.JS{},
+          doc: """
+          An additional `Phoenix.LiveView.JS` command to execute when the dialog
+          is canceled. This command is executed in addition to closing the dialog. If
+          you only want the dialog to be closed, you don't have to set this attribute.
+          """
+
+        attr :dismissable, :boolean,
+          default: false,
+          doc: """
+          When set to `true`, the dialog can be dismissed by clicking a close button
+          or by pressing the escape key.
+          """
+
+        attr :close_label, :string,
+          default: "Close",
+          doc: "Aria label for the close button."
+
+        slot :title, required: true
+        slot :inner_block, required: true, doc: "The modal body."
+
+        slot :close,
+          doc: "The content for the 'close' link. Defaults to the word 'close'."
+
+        slot :footer
+
+        attr :rest, :global, doc: "Any additional HTML attributes."
+      end,
+    heex:
+      quote do
+        ~H"""
+        <dialog
+          id={@id}
+          role="alertdialog"
+          class={[@base_class | @modifier_classes]}
+          aria-modal={(@open && "true") || "false"}
+          aria-labelledby={"#{@id}-title"}
+          aria-describedby={"#{@id}-content"}
+          open={@open}
+          phx-mounted={@open && Doggo.show_modal(@id)}
+          phx-remove={Doggo.hide_modal(@id)}
+          data-cancel={JS.exec(@on_cancel, "phx-remove")}
+          {@rest}
+        >
+          <.focus_wrap
+            id={"#{@id}-container"}
+            class="alert-dialog-container"
+            phx-window-keydown={
+              @dismissable && Phoenix.LiveView.JS.exec("data-cancel", to: "##{@id}")
+            }
+            phx-key={@dismissable && "escape"}
+            phx-click-away={
+              @dismissable && Phoenix.LiveView.JS.exec("data-cancel", to: "##{@id}")
+            }
+          >
+            <section>
+              <header>
+                <button
+                  :if={@dismissable}
+                  href="#"
+                  class="alert-dialog-close"
+                  aria-label={@close_label}
+                  phx-click={JS.exec("data-cancel", to: "##{@id}")}
+                >
+                  <%= render_slot(@close) %>
+                  <span :if={@close == []}>close</span>
+                </button>
+                <h2 id={"#{@id}-title"}><%= render_slot(@title) %></h2>
+              </header>
+              <div id={"#{@id}-content"} class="alert-dialog-content">
+                <%= render_slot(@inner_block) %>
+              </div>
+              <footer :if={@footer != []}>
+                <%= render_slot(@footer) %>
+              </footer>
+            </section>
+          </.focus_wrap>
+        </dialog>
         """
       end
   )
@@ -1385,7 +1544,7 @@ defmodule Doggo.Components do
 
     This component is appropriate for non-critical interactions. For dialogs
     requiring immediate user response, such as confirmations or warnings, use
-    `Doggo.alert_dialog/1` instead.
+    `.alert_dialog/1` instead.
     """,
     usage: """
     There are two primary ways to manage the display of the modal: via URL state
--- test/doggo/components_test.exs
+++ test/doggo/components_test.exs
@@ -17,6 +17,7 @@ defmodule Doggo.ComponentsTest do
 
     accordion()
     action_bar()
+    alert_dialog()
     badge()
     box()
     breadcrumb()
@@ -207,6 +208,123 @@ defmodule Doggo.ComponentsTest do
         """)
 
       assert attribute(html, "div", "data-what") == "ever"
+    end
+  end
+
+  describe "alert_dialog/1" do
+    test "default" do
+      assigns = %{}
+
+      html =
+        parse_heex(~H"""
+        <TestComponents.alert_dialog id="pet-alert" on_cancel={JS.push("cancel")}>
+          <:title>Edit dog</:title>
+          dog-form
+          <:footer>paw</:footer>
+        </TestComponents.alert_dialog>
+        """)
+
+      dialog = find_one(html, "dialog:root")
+      assert attribute(dialog, "role") == "alertdialog"
+      assert attribute(dialog, "id") == "pet-alert"
+      assert attribute(dialog, "class") == "alert-dialog"
+      assert attribute(dialog, "aria-modal") == "false"
+      assert attribute(dialog, "open") == nil
+      assert attribute(dialog, "phx-mounted") == nil
+
+      assert Floki.find(html, ".alert-dialog-close") == []
+
+      h2 = find_one(html, ":root > div > section > header > h2")
+      assert text(h2) == "Edit dog"
+
+      assert text(html, "section > .alert-dialog-content") == "dog-form"
+      assert text(html, "section > footer") == "paw"
+    end
+
+    test "dismissable" do
+      assigns = %{}
+
+      html =
+        parse_heex(~H"""
+        <TestComponents.alert_dialog
+          id="pet-alert"
+          on_cancel={JS.push("cancel")}
+          dismissable
+        >
+          <:title>Edit dog</:title>
+          dog-form
+          <:footer>paw</:footer>
+        </TestComponents.alert_dialog>
+        """)
+
+      a =
+        find_one(
+          html,
+          ":root > div > section > header > button.alert-dialog-close"
+        )
+
+      assert attribute(a, "aria-label") == "Close"
+      assert text(a, "span") == "close"
+    end
+
+    test "opened" do
+      assigns = %{}
+
+      html =
+        parse_heex(~H"""
+        <TestComponents.alert_dialog id="pet-alert" open>
+          <:title>Edit dog</:title>
+          dog-form
+        </TestComponents.alert_dialog>
+        """)
+
+      dialog = find_one(html, "dialog:root")
+      assert attribute(dialog, "aria-modal") == "true"
+      assert attribute(dialog, "open") == "open"
+    end
+
+    test "with close slot" do
+      assigns = %{}
+
+      html =
+        parse_heex(~H"""
+        <TestComponents.alert_dialog id="pet-alert" dismissable>
+          <:title>Edit dog</:title>
+          dog-form
+          <:close>X</:close>
+        </TestComponents.alert_dialog>
+        """)
+
+      assert text(html, "button.alert-dialog-close") == "X"
+    end
+
+    test "with close label" do
+      assigns = %{}
+
+      html =
+        parse_heex(~H"""
+        <TestComponents.alert_dialog id="pet-alert" close_label="Cancel" dismissable>
+          <:title>Edit dog</:title>
+          dog-form
+        </TestComponents.alert_dialog>
+        """)
+
+      assert attribute(html, "button.alert-dialog-close", "aria-label") ==
+               "Cancel"
+    end
+
+    test "with global attribute" do
+      assigns = %{}
+
+      html =
+        parse_heex(~H"""
+        <TestComponents.alert_dialog id="pet-alert" data-test="hello">
+          <:title>Edit dog</:title>
+          dog-form
+        </TestComponents.alert_dialog>
+        """)
+
+      assert attribute(html, ":root", "data-test") == "hello"
     end
   end
 
