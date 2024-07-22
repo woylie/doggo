6143e385dfa07e35a758acdac24a67a4ff247854
woylie <13847569+woylie@users.noreply.github.com>
2024-07-22 22:59:27 +0900

    fix tree story with dependent component

--- lib/doggo/storybook/shared.ex
+++ lib/doggo/storybook/shared.ex
@@ -9,7 +9,7 @@ defmodule Doggo.Storybook.Shared do
     end
   end
 
-  def icon_svg(:add) do
+  defp icon_svg(:add) do
     """
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -25,6 +25,67 @@ defmodule Doggo.Storybook.Shared do
     >
       <path d="M5 12h14"/>
       <path d="M12 5v14"/>
+    </svg>
+    """
+  end
+
+  defp icon_svg(:folder) do
+    """
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="lucide lucide-folder"
+    >
+      <path d="M20 20a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.9a2 2 0 0 1-1.69-.9L9.6 3.9A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2Z" />
+    </svg>
+    """
+  end
+
+  defp icon_svg(:paw) do
+    """
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="lucide lucide-paw-print"
+    >
+      <circle cx="11" cy="4" r="2" />
+      <circle cx="18" cy="8" r="2" />
+      <circle cx="20" cy="16" r="2" />
+      <path d="M9 10a5 5 0 0 1 5 5v3.5a3.5 3.5 0 0 1-6.84 1.045Q6.52 17.48 4.46 16.84A3.5 3.5 0 0 1 5.5 10Z" />
+    </svg>
+    """
+  end
+
+  defp icon_svg(:like) do
+    """
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="lucide lucide-heart"
+      aria-hidden="true"
+    >
+      <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
     </svg>
     """
   end
