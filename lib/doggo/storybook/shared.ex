080b180bdd6b4cfe1d764f0783774f81aaa79e7f
woylie <13847569+woylie@users.noreply.github.com>
2024-07-22 23:05:20 +0900

    add icon variant to alert story

--- lib/doggo/storybook/shared.ex
+++ lib/doggo/storybook/shared.ex
@@ -48,6 +48,26 @@ defmodule Doggo.Storybook.Shared do
     """
   end
 
+  defp icon_svg(:info) do
+    """
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="lucide lucide-info"
+    >
+      <circle cx="12" cy="12" r="10"/><path d="M12 16v-4"/>
+      <path d="M12 8h.01"/>
+    </svg>
+    """
+  end
+
   defp icon_svg(:paw) do
     """
     <svg
