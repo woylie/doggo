5c73408d48000cc45b898dd88bfc6db22bdd990c
woylie <13847569+woylie@users.noreply.github.com>
2024-07-22 18:25:42 +0900

    format generated story modules

--- demo/storybook/menu/menu_item_radio_group.story.exs
+++ demo/storybook/menu/menu_item_radio_group.story.exs
@@ -1,4 +1,7 @@
 defmodule Storybook.Components.MenuItemRadioGroup do
   use PhoenixStorybook.Story, :component
-  use Doggo.Storybook, module: DemoWeb.CoreComponents, name: :menu_item_radio_group
+
+  use Doggo.Storybook,
+    module: DemoWeb.CoreComponents,
+    name: :menu_item_radio_group
 end
