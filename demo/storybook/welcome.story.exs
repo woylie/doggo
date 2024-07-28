de5c38d059381d62c3cfbb36b295ca945fc1131b
woylie <13847569+woylie@users.noreply.github.com>
2024-07-28 10:43:37 +0900

    update storybook welcome page

--- demo/storybook/welcome.story.exs
+++ demo/storybook/welcome.story.exs
@@ -8,15 +8,19 @@ defmodule Storybook.MyPage do
   def render(assigns) do
     ~H"""
     <div class="psb-welcome-page">
-      <p>
-        This is the storybook of the Doggo demo application. All components were
-        generated with the default options. The stories were generated with <code>mix dog.gen.stories -m DemoWeb.CoreComponents -o storybook</code>.
-      </p>
+      <p>Doggo is a headless component library for Phoenix LiveView. <em>Headless UI</em> refers to components that provide the logic and structure needed to build user interfaces but leave the styling up to you.</p>
+      <p>This storybook presents the components with their default options. Some components already include example styles, while others remain unstyled and will receive example styles in the future.</p>
+      <p>You can override the default options, including component names, base classes, and modifier attributes such as button sizes or variants.</p>
+      <p>The library includes a mix task to generate storybook stories for the components configured in your application.</p>
+      <p>For details about component maturity and configuration options, please refer to the <a href="https://hexdocs.pm/doggo/Doggo.Components.html" target="_blank">Components documentation</a></p>
 
-      <p>
-        Keep in mind that Doggo is headless, which means that it doesn't come
-        with any default styles.
-      </p>
+      <h2>Resources</h2>
+      <ul>
+        <li><a href="https://github.com/woylie/doggo" target="_blank">Github repository</a></li>
+        <li><a href="https://hexdocs.pm/doggo/readme.html" target="_blank">Installation and usage</a></li>
+        <li><a href="https://hexdocs.pm/doggo/Doggo.Components.html" target="_blank">Components documentation</a></li>
+        <li><a href="https://hexdocs.pm/doggo/Mix.Tasks.Dog.Gen.Stories.html" target="_blank">Story generator task</a></li>
+      </ul>
     </div>
     """
   end
