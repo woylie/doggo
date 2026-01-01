1d6be97e078c70b9b6458c31370dc5a71ab09799
woylie <13847569+woylie@users.noreply.github.com>
2026-01-01 15:59:15 +0100

    fix warnings

--- demo/lib/demo_web/endpoint.ex
+++ demo/lib/demo_web/endpoint.ex
@@ -12,7 +12,8 @@ defmodule DemoWeb.Endpoint do
   ]
 
   socket "/live", Phoenix.LiveView.Socket,
-    websocket: [connect_info: [session: @session_options]]
+    websocket: [connect_info: [session: @session_options]],
+    longpoll: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
