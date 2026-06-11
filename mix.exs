6c29d6d487686b245e253cc3028866319acc67a5
woylie <13847569+woylie@users.noreply.github.com>
2026-06-11 11:43:37 +0200

    require phoenix_live_view ~> 1.1

--- mix.exs
+++ mix.exs
@@ -62,7 +62,7 @@ defmodule Doggo.MixProject do
       {:jason, "1.4.5", only: [:dev, :test]},
       {:lazy_html, "0.1.11", only: :test},
       {:makeup_diff, "0.1.1", only: :dev},
-      {:phoenix_live_view, "~> 1.1.0"},
+      {:phoenix_live_view, "~> 1.1"},
       {:phoenix_storybook, "~> 1.1"},
       {:tzdata, "1.1.3", only: [:test]}
     ]
