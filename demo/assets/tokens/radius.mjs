8f79fd91db8cef6f24df9ec6f2ce96b0836fcaa8
woylie <13847569+woylie@users.noreply.github.com>
2026-09-07 18:16:17 +0200

    remove remaining scss, remove style dictionary

--- demo/assets/tokens/radius.mjs
+++
@@ -1,9 +0,0 @@
-export default {
-  radius: {
-    $type: "dimension",
-    s: { $value: "4px" },
-    m: { $value: "8px" },
-    circle: { $value: "50%" },
-    pill: { $value: "999rem" },
-  },
-};
