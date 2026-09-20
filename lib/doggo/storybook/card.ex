b7d613e5d5cafe74843bbeebaca2dcbaae50bc5d
woylie <13847569+woylie@users.noreply.github.com>
2026-09-20 17:12:08 +0200

    improve card story

--- lib/doggo/storybook/card.ex
+++ lib/doggo/storybook/card.ex
@@ -16,42 +16,61 @@ defmodule Doggo.Storybook.Card do
     [
       %Variation{
         id: :default,
-        slots: slots()
+        slots: [image(), header(), body(), footer()]
+      },
+      %Variation{
+        id: :without_image,
+        slots: [header(), body(), footer()]
+      },
+      %Variation{
+        id: :without_footer,
+        slots: [image(), header(), body()]
+      },
+      %Variation{
+        id: :body_only,
+        slots: [body()]
       }
     ]
   end
 
   def modifier_variation_base(_id, _name, _value, _opts) do
     %{
-      slots: slots()
+      slots: [image(), header(), body(), footer()]
     }
   end
 
-  defp slots do
-    [
-      """
-      <:image>
-        <img
-          src="https://github.com/woylie/doggo/blob/main/assets/images/dog_1.webp?raw=true"
-          alt="A gray-muzzled dog in a camouflage coat and harness."
-        />
-      </:image>
-      """,
-      """
-      <:body>
-        The next dog fashion show is coming up quickly. Here's what you need
-        to look out for.
-      </:body>
-      """,
-      """
-      <:header><h2>Dog Fashion Show</h2></:header>
-      """,
-      """
-      <:footer>
-        <span>2023-11-15 12:24</span>
-        <span>Events</span>
-      </:footer>
-      """
-    ]
+  defp image do
+    """
+    <:image>
+      <img
+        src="https://github.com/woylie/doggo/blob/main/assets/images/dog_1.webp?raw=true"
+        alt="A gray-muzzled dog in a camouflage coat and harness."
+      />
+    </:image>
+    """
+  end
+
+  defp header do
+    """
+    <:header><h2>Dog Fashion Show</h2></:header>
+    """
+  end
+
+  defp body do
+    """
+    <:body>
+      The next dog fashion show is coming up quickly. Here's what you need
+      to look out for.
+    </:body>
+    """
+  end
+
+  defp footer do
+    """
+    <:footer>
+      <span>2023-11-15 12:24</span>
+      <span>Events</span>
+    </:footer>
+    """
   end
 end
